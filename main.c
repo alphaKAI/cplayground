dd2a88b0c1f0ab99119a5bcd173aed5efdf71d66
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2019-06-15 22:02:35 +0900

    change main function for minilisp developping

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+#include "cplayground.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <gc.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Dummy\n");
+  if (argc < 2) {
+    fprintf(stderr, "usage: ./%s prog_path\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  gc_init();
+  vm_init();
+  sds code = readText(sdsnew(argv[1]));
+  Vector *parsed = sexp_parse(code);
+  Vector *compiled = vm_compile(parsed);
+  printf("compiled instructions...\n");
+  vm_ins_dump(compiled);
+  vm_exec(compiled);
   return 0;
 }
