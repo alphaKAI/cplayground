498f8913257aec23f14477d6220e212d7ae011a5
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2019-06-16 21:35:46 +0900

    add number manipulate utility

--- util.h
+++ util.h
@@ -1,6 +1,7 @@
 #ifndef __UTIL_HEADER_INCLUDED__
 #define __UTIL_HEADER_INCLUDED__
 #include "sds/sds.h"
+#include <limits.h>
 
 typedef void (*ELEM_DESTRUCTOR)(void *);
 typedef int (*ELEM_COMPARE)(void *, void *);
@@ -37,4 +38,41 @@ typedef struct {
 
 sds readText(const sds file_name);
 
+#define GenNumericToLowers(T, R, T_NAME, R_NAME, R_MAX)                        \
+  static inline R *T_NAME##_to_##R_NAME##s(T v) {                              \
+    size_t T_size = sizeof(T);                                                 \
+    size_t R_size = sizeof(R);                                                 \
+    size_t rets_length = T_size / R_size;                                      \
+    R *rets = xmalloc(sizeof(R) * rets_length);                                \
+                                                                               \
+    for (size_t i = 0; i < rets_length; i++) {                                 \
+      R t = (v >> (R_size * 8 * i)) & R_MAX;                                   \
+      rets[i] = t;                                                             \
+    }                                                                          \
+                                                                               \
+    return rets;                                                               \
+  }
+
+#define GenLowersToNumeric(T, R, T_NAME, R_NAME)                               \
+  static inline R T_NAME##s_to_##R_NAME(T *inputs) {                           \
+    R v = 0;                                                                   \
+    size_t T_size = sizeof(T);                                                 \
+    size_t R_size = sizeof(R);                                                 \
+    size_t iter_end = R_size / T_size;                                         \
+                                                                               \
+    for (size_t i = 0; i < iter_end; i++) {                                    \
+      v |= ((R)inputs[i]) << (T_size * 8 * i);                                 \
+    }                                                                          \
+                                                                               \
+    return v;                                                                  \
+  }
+
+// int <-> unsigned char
+GenNumericToLowers(int, unsigned char, int, uchar, UCHAR_MAX);
+GenLowersToNumeric(unsigned char, int, uchar, int);
+
+// unsigned iong long int <-> unsigned char
+GenNumericToLowers(unsigned long long int, unsigned char, ulli, uchar,
+                   UCHAR_MAX);
+GenLowersToNumeric(unsigned char, unsigned long long int, uchar, ulli);
 #endif
